651ca852a55d097e82a3c4527b1290066e09d49d
vinaythoutam <vnnu235@gmail.com>
2017-10-16 16:24:11 +0530

    Commited

---
+++ app/src/main/java/com/example/vinay/resumebuilder/model/AcademicInfo.java
@@ -0,0 +1,91 @@
+package com.example.vinay.resumebuilder.model;
+
+/**
+ * Created by Vinay on 12-10-2017.
+ */
+
+public class AcademicInfo {
+    String gname, gyear, gpercentage, sname, syear, spercentage, cname, cyear, cpercentage;
+    int cid;
+
+    public String getGname() {
+        return gname;
+    }
+
+    public String getGyear() {
+        return gyear;
+    }
+
+    public String getGpercentage() {
+        return gpercentage;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public String getSyear() {
+        return syear;
+    }
+
+    public String getSpercentage() {
+        return spercentage;
+    }
+
+    public String getCname() {
+        return cname;
+    }
+
+    public String getCyear() {
+        return cyear;
+    }
+
+    public String getCpercentage() {
+        return cpercentage;
+    }
+
+    public int getCid() {
+        return cid;
+    }
+
+
+    public void setGname(String gname) {
+        this.gname = gname;
+    }
+
+    public void setGyear(String gyear) {
+        this.gyear = gyear;
+    }
+
+    public void setGpercentage(String gpercentage) {
+        this.gpercentage = gpercentage;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public void setSyear(String syear) {
+        this.syear = syear;
+    }
+
+    public void setSpercentage(String spercentage) {
+        this.spercentage = spercentage;
+    }
+
+    public void setCname(String cname) {
+        this.cname = cname;
+    }
+
+    public void setCyear(String cyear) {
+        this.cyear = cyear;
+    }
+
+    public void setCpercentage(String cpercentage) {
+        this.cpercentage = cpercentage;
+    }
+
+    public void setCid(int cid) {
+        this.cid = cid;
+    }
+}
